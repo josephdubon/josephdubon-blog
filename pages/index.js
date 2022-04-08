f7a22a1523fdbe3411b9f4c37626c7e5e778b786
Joseph Dubon <josephdubon@pm.me>
2022-04-07 18:07:52 -0700

    added new layout to blog roll with new Date component to render date visually nicer

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import {getSortedPostsData} from '../lib/posts'
+import Date from '../components/date'
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData()
@@ -29,17 +31,19 @@ export default function Home({allPostsData}) {
 
         {/* blog posts list */}
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-            <h2 className={utilStyles.headingLg}>Journal</h2>
+            <h2 className={utilStyles.headingLg}>Blog Roll</h2>
 
             <ul className={utilStyles.list}>
                 {/* loop through posts and list by date */}
                 {allPostsData.map(({id, date, title}) => (
                     <li className={utilStyles.listItem} key={id}>
-                        {title}
+                        <Link href={`/posts/${id}`}>
+                            <a>{title}</a>
+                        </Link>
                         <br/>
-                        {id}
-                        <br/>
-                        {date}
+                        <small className={utilStyles.lightText}>
+                            <Date dateString={date}/>
+                        </small>
                     </li>
                 ))}
             </ul>
