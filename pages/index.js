c606edef1a5e2229c2e207772a53b9e53fcc052a
Joseph Dubon <josephdubon@pm.me>
2022-04-07 17:01:20 -0700

    added posts list render with new getStaticProps function getting posts with getSortedPostsData

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,48 @@
 import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import {getSortedPostsData} from '../lib/posts'
 
-export default function Home() {
-    return (
-        <Layout home>
-            <Head>
-                <title>{siteTitle}</title>
-            </Head>
-            <section className={utilStyles.headingMd}>
-                {/* introduction */}
-                <p>Hello friends, my name is Joseph Dubon. I am a fullstack software engineer, portrait/lifestyle
-                    photography, and musician.</p>
-                <p>
-                    Feel free to explore my website and discover my favorite ways of expression, code (full stack) and art (photos and music).
-                </p>
-            </section>
-        </Layout>
-    )
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
+export default function Home({allPostsData}) {
+    return (<Layout home>
+        <Head>
+            <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+            {/* introduction */}
+            <p>Hello friends, my name is Joseph Dubon. I am a fullstack software engineer, portrait/lifestyle
+                photography, and musician.</p>
+            <p>
+                Feel free to explore my website and discover my favorite ways of expression, code (full stack) and
+                art (photos and music).
+            </p>
+        </section>
+
+        {/* blog posts list */}
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+            <h2 className={utilStyles.headingLg}>Journal</h2>
+
+            <ul className={utilStyles.list}>
+                {/* loop through posts and list by date */}
+                {allPostsData.map(({id, date, title}) => (
+                    <li className={utilStyles.listItem} key={id}>
+                        {title}
+                        <br/>
+                        {id}
+                        <br/>
+                        {date}
+                    </li>
+                ))}
+            </ul>
+        </section>
+    </Layout>)
 }
