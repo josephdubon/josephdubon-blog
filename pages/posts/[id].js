94588cf28a789fd30442e1465d8546816e2b35cc
Joseph Dubon <josephdubon@pm.me>
2022-04-07 17:13:50 -0700

    added logic to getStaticPaths, moved getStaticPaths and getStaticProps above the Post function

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,19 @@
 import Layout from '../../components/layout'
+import {getAllPostIds} from '../../lib/posts'
+
+export async function getStaticPaths() {
+    // return a list of possible value for id
+    const paths = getAllPostIds()
+    return {
+        paths, // contains known paths returned by getAllPostIds(), include  params defined by pages/posts/[id].js
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}) {
+    // fetch required data for the blog post using params.id
+
+}
 
 export default function Post() {
     return <Layout>
