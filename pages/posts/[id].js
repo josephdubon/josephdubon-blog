81eeced0aae21d589abf234ebaaab1ef0fb92217
Joseph Dubon <josephdubon@pm.me>
2022-04-10 23:23:33 -0700

    added placeholder content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Layout from '../../components/layout'
-import Date from '../../components/date'
+import {Divider, FlexboxGrid, Placeholder, Row} from 'rsuite'
+import {useState} from 'react'
 import {getAllPostIds, getPostData} from '../../lib/posts'
-import utilStyles from '../../styles/utils.module.css'
+import Date from '../../components/date'
+
+const {Paragraph} = Placeholder
 
 export async function getStaticPaths() {
     // return a list of possible value for id
@@ -25,18 +28,42 @@ export async function getStaticProps({params}) {
     }
 }
 
-export default function Post({postData}) {
-    return (<Layout>
+export default function CodeHome({postData}) {
+    // use state
+    const [open, setOpen] = useState(false)
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
+
+    return (<Layout home>
         <Head>
             <title>{postData.title}</title>
         </Head>
-        <article>
-            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-            <div className={utilStyles.lightText}>
-                <Date dateString={postData.date}/>
-            </div>
-            <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
-        </article>
-    </Layout>)
-}
 
+        <FlexboxGrid justify='center'>
+            {/* Section - hello */}
+            <FlexboxGrid.Item colspan={20}>
+                <Row>
+                    <Divider>Joseph Dubon</Divider>
+                    <h2>{postData.title}</h2>
+                    <small>{postData.name}</small>
+                    <small><Date dateString={postData.date}/></small>
+                </Row>
+                <hr/>
+                <Row>
+                    {/* Section  - about me */}
+                    <FlexboxGrid justify='space-around'>
+                        <FlexboxGrid.Item colspan={24}>
+                            <article>
+                                <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+                            </article>
+                        </FlexboxGrid.Item>
+                    </FlexboxGrid>
+                </Row>
+
+            </FlexboxGrid.Item>
+
+        </FlexboxGrid>
+
+    </Layout>)
+
+}
