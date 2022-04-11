281c8765513fa989414e4518342cf2cce93b8d52
Joseph Dubon <josephdubon@pm.me>
2022-04-10 20:52:38 -0700

    created new component for art page

---
+++ pages/art.js
@@ -0,0 +1,17 @@
+import Layout from '../components/layout'
+
+export default function ArtHome() {
+    return (<Layout>
+        <h1>Art Home</h1>
+        <p>What Is Lorem Ipsum Used for?
+            Lorem ipsum is a popular form of dummy text used in the publishing industry (and later all forms of digital
+            design) since the 16th century.
+
+            Its main purpose is to help visualize the layout and style of a document without the need for actual
+            content.
+
+            Apart from its use as placeholder text before the real content is at hand, lorem ipsum can also be used
+            after the content is available – by temporarily replacing it to separate form from meaning in a design. This
+            excercise is sometimes called greeking – not to be confused with geeking 🤓</p>
+    </Layout>)
+}
