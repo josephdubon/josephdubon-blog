3ea85716b1fd3844d5aa9e0fa2873dc00f7d5d5f
Joseph Dubon <josephdubon@pm.me>
2022-04-10 20:51:15 -0700

    removed posts list

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,6 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import {getSortedPostsData} from '../lib/posts'
-import Date from '../components/date'
-
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
-    return {
-        props: {
-            allPostsData
-        }
-    }
-}
 
 export default function Home({allPostsData}) {
     return (<Layout home>
@@ -29,24 +17,5 @@ export default function Home({allPostsData}) {
             </p>
         </section>
 
-        {/* blog posts list */}
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-            <h2 className={utilStyles.headingLg}>Blog Roll</h2>
-
-            <ul className={utilStyles.list}>
-                {/* loop through posts and list by date */}
-                {allPostsData.map(({id, date, title}) => (
-                    <li className={utilStyles.listItem} key={id}>
-                        <Link href={`/posts/${id}`}>
-                            <a>{title}</a>
-                        </Link>
-                        <br/>
-                        <small className={utilStyles.lightText}>
-                            <Date dateString={date}/>
-                        </small>
-                    </li>
-                ))}
-            </ul>
-        </section>
     </Layout>)
 }
