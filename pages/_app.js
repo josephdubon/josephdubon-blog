73e7760d7de20c24f8cc7c7588e81117dd0ecfda
Joseph Dubon <josephdubon@pm.me>
2022-04-10 17:12:25 -0700

    added rsuite css

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 // You can use this App component to keep state when navigating between pages, for example.
 
 import '../styles/global.css'
+import 'rsuite/dist/rsuite.min.css'
 
 export default function App({Component, pageProps}) {
     return <Component {...pageProps} />
