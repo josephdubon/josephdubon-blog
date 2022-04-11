12e9129366ffd17396871b6f9eb11de8e7ef88da
Joseph Dubon <josephdubon@pm.me>
2022-04-10 21:47:50 -0700

    updated meta for page

--- components/layout.js
+++ components/layout.js
@@ -7,25 +7,23 @@ const name = 'Joseph Dubon'
 export const siteTitle = name + 'Full-Stack Software Engineer'
 
 export default function Layout({children, home}) {
-    return (
-        <div className={styles.container}>
-
-            <Head>
-                <link rel='icon' href='/favicon.ico'/>
-                <meta
-                    name='description'
-                    content='Joseph Dubon is a fullstack software engineer. Skills ranging from web development, database design, dev-ops, root cause analysis.'
-                />
-                {/* meta tags (like og:image), which are used to describe a page's content */}
-                <meta
-                    property='og:image'
-                    content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
-                <meta name='og:title' content={siteTitle}/>
-                <meta name='twitter:card' content='summary_large_image'/>
-            </Head>
+    return (<>
+        {/* metadata */}
+        <Head>
+            <link rel='icon' href='/favicon.ico'/>
+            <meta
+                name='description'
+                content='Joseph Dubon is a fullstack software engineer. Skills ranging from web development, database design, dev-ops, root cause analysis.'
+            />
+            {/* meta tags (like og:image), which are used to describe a page's content */}
+            <meta
+                property='og:image'
+                content={`https://og-image.vercel.app/${encodeURI(siteTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+            />
+            <meta name='og:title' content={siteTitle}/>
+            <meta name='twitter:card' content='summary_large_image'/>
+            <title>{' '}</title>
+        </Head>
 
             <NavBar />
 
