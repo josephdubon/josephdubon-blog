2857b8e52ff9ad9baf79ab07ca045638f93224d1
Joseph Dubon <josephdubon@pm.me>
2022-04-10 21:46:28 -0700

    cleaned up homepage with just some basic intro info

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,64 @@
+import {useState} from 'react'
 import Head from 'next/head'
+import {Button, ButtonToolbar, Divider, FlexboxGrid, List, Modal, Row} from 'rsuite'
 import Layout, {siteTitle} from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
 
-export default function Home({allPostsData}) {
+export default function Home() {
+    // use state
+    const [open, setOpen] = useState(false)
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
+
     return (<Layout home>
         <Head>
             <title>{siteTitle}</title>
         </Head>
-        <section className={utilStyles.headingMd}>
-            {/* introduction */}
-            <p>Hello friends, my name is Joseph Dubon. I am a fullstack software engineer, portrait/lifestyle
-                photography, and musician.</p>
-            <p>
-                Feel free to explore my website and discover my favorite ways of expression, code (full stack) and
-                art (photos and music).
-            </p>
-        </section>
+
+        <FlexboxGrid justify='center'>
+            {/* Section - hello */}
+            <FlexboxGrid.Item colspan={20}>
+                <Row>
+                    <Divider>hello</Divider>
+                    <h2>My name is Joseph Adonay Dubon</h2>
+                    <h4>I am a full-stack software engineer</h4>
+                    <p>BUILD THE WEB. SECURE THE FUTURE. CAPTURE TODAY, SO WE WILL NOT FORGET
+                        TOMORROW.
+                    </p>
+
+                    {/* Section  - modal - button */}
+                    <div className='modal-container'>
+                        <br/>
+                        <ButtonToolbar>
+                            <Button onClick={handleOpen}> Contact Card</Button>
+                        </ButtonToolbar>
+
+                        {/* Section - modal - content */}
+                        <Modal open={open} onClose={handleClose}>
+                            <Modal.Header>
+                                <Modal.Title>Contact Card</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <List bordered hover>
+                                    <List.Item>Name: Joseph Dubon</List.Item>
+                                    <List.Item>Title: Full-Stack Software Engineer</List.Item>
+                                    <List.Item>Email: <a
+                                        href='mailto:jd@josephdubon.com'>jd@josephdubon.com</a></List.Item>
+                                    <List.Item>Phone: <a
+                                        href='tel:(775)232-6280'>(775)232-6280</a></List.Item>
+                                </List>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button onClick={handleClose} appearance='primary'>
+                                    Close
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    </div>
+                </Row>
+
+            </FlexboxGrid.Item>
+
+        </FlexboxGrid>
 
     </Layout>)
 }
