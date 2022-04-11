8a464822737b5d7200ac4c28e3390e1d18dce7b1
Joseph Dubon <josephdubon@pm.me>
2022-04-10 20:50:42 -0700

    added rsuite added rsuite CustomProvider to app for use in all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 // This App component is the top-level component which will be common across all the different pages.
 // You can use this App component to keep state when navigating between pages, for example.
 
-import '../styles/global.css'
 import 'rsuite/dist/rsuite.min.css'
+import '../styles/global.css'
+
+import {CustomProvider} from 'rsuite'
 
 export default function App({Component, pageProps}) {
-    return <Component {...pageProps} />
+    return (<CustomProvider theme='dark'>
+        <Component {...pageProps} />
+    </CustomProvider>)
 }
