5da6e0d129f2708938754ddbd47c30189b098655
Joseph Dubon <josephdubon@pm.me>
2022-04-13 23:05:36 -0700

    added pulRight nav with github and linkedin links, updated navbar items to match new site structure

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import {Nav, Navbar} from 'rsuite'
 import {forwardRef} from 'react'
 
-export default function NavBarInstance({onSelect, activeKey}) {
+export default function NavBarInstance() {
 
     // There are some navigation components in the rsuite component, such as Dropdown, Nav, Breadcrumb,
     // which are used in conjunction with the Link component of Next.js to use the as prop.
@@ -18,26 +18,36 @@ export default function NavBarInstance({onSelect, activeKey}) {
 
     return (<>
         <Navbar appearance='inverse'>
-            <Navbar.Brand as={NavLink} href='/'>
-                JSPH/DBN ⌘
-            </Navbar.Brand>
-            <Nav>
-                <Nav.Item as={NavLink} href='/about'>
-                    About
-                </Nav.Item>
+            <section className='mainNav'>
+                <Navbar.Brand as={NavLink} href='/'>
+                    JSPH/DBN ⌘
+                </Navbar.Brand>
+                <Nav>
+                    <Nav.Item as={NavLink} href='/about'>
+                        About
+                    </Nav.Item>
 
-                <Nav.Item as={NavLink} href='/posts/posts-list'>
-                    Posts
-                </Nav.Item>
+                    <Nav.Item as={NavLink} href='/code-and-development'>
+                        Code & Dev
+                    </Nav.Item>
 
-                <Nav.Item as={NavLink} href='/code'>
-                    Code
-                </Nav.Item>
+                    <Nav.Item as={NavLink} href='/live-projects'>
+                        Live Projects
+                    </Nav.Item>
 
-                <Nav.Item as={NavLink} href='/photo'>
-                    Photo
-                </Nav.Item>
-            </Nav>
+                    <Nav.Item as={NavLink} href='/art'>
+                        Art
+                    </Nav.Item>
+                </Nav>
+                <Nav pullRight>
+                    <Nav.Item href='https://github.com/josephdubon'>
+                        GitHub
+                    </Nav.Item>
+                    <Nav.Item href='https://www.linkedin.com/in/joseph-dubon/'>
+                        LinkedIn
+                    </Nav.Item>
+                </Nav>
+            </section>
         </Navbar>
     </>)
 }
