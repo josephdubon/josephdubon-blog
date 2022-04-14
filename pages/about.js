d38b129ae4de4c6517b5f00381acd8fe462a664a
Joseph Dubon <josephdubon@pm.me>
2022-04-13 23:02:01 -0700

    rearranged layout to match other pages

--- pages/about.js
+++ pages/about.js
@@ -26,84 +26,64 @@ export default function AboutHome({allPostsData}, props) {
                     </small>
                     <br/>
                     <br/>
-                    <p>Website in development, thank you for your patience</p>
-
-                    {/* Section  - modal - button */}
-                    <div className='modal-container'>
-                        <br/>
-                        <ButtonToolbar>
-                            <Button onClick={handleOpen}> Contact Card</Button>
-                        </ButtonToolbar>
-
-                        {/* Section - modal - content */}
-                        <Modal open={open} onClose={handleClose}>
-                            <Modal.Header>
-                                <Modal.Title>Contact Card</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <List bordered hover>
-                                    <List.Item>Name: Joseph Dubon</List.Item>
-                                    <List.Item>Title: Full-Stack Software Engineer</List.Item>
-                                    <List.Item>Email: <a
-                                        href='mailto:jd@josephdubon.com'>jd@josephdubon.com</a></List.Item>
-                                    <List.Item>Phone: <a
-                                        href='tel:(775)232-6280'>(775)232-6280</a></List.Item>
-                                </List>
-                            </Modal.Body>
-                            <Modal.Footer>
-                                <Button onClick={handleClose} appearance='primary'>
-                                    Close
-                                </Button>
-                            </Modal.Footer>
-                        </Modal>
-                    </div>
-                </Row>
+                    <Link href='#'>
+                        <a onClick={handleOpen}> Contact Card</a>
+                    </Link>
+                </Col>
+                {/* Section  - modal - button */}
                 <Row>
-                    {/* Section  - about me */}
-                    <Divider>about me</Divider>
-                    <FlexboxGrid justify='space-around'>
-                        <FlexboxGrid.Item colspan={14}>
-                            <h6>I started my professional career as a developer in 2012</h6>
-                            <p>But I have been programming and building/breaking computers for most my life. I built my
-                                first computer from recycled and donated parts when I was in elementary school. I built
-                                my first website just a few years later.</p>
-                            <br/>
+                    {/* send message if there are no posts */}
+                    {posts.length < 1 ? ('There are no posts here yet. Thank you for your patience as I build out my website.') : ('')}
 
-                            <h6>I am passionate about tech, education, design,
-                                photography</h6>
-                            <p>and of course improving the human condition with the small bits of wisdom <br/>life
-                                experience and education have granted me. I also love to play music with friends and
-                                jam with my kids.
-                            </p>
-                            <br/>
-
-                            <h6>I love learning and adapting to new tech stacks and environments</h6>
-                            <p>my favorite programming stacks are with Python, Django, and Javascript. I also use Emacs
-                                Elisp with Doom as my personal bullet journal and engineers logbook.</p>
-                            <br/>
-
-                            <h6>Nothing is 'hack proof' but...</h6>
-                            <p>we can be diligent on how we build, technologies we use, as well as the <br/>processes we
-                                implement to maintain our projects.</p>
-                        </FlexboxGrid.Item>
-
-                        <FlexboxGrid.Item colspan={6}>
-                            <Timeline>
-                                <Timeline.Item>2012 - Wordpress Web Developer</Timeline.Item>
-                                <Timeline.Item>2015 - Jr Web Developer</Timeline.Item>
-                                <Timeline.Item>2019 - Frontend Web Developer</Timeline.Item>
-                                <Timeline.Item>2021 - Fullstack Software
-                                    Engineer</Timeline.Item>
-                                <Timeline.Item> 2022 - What a wonderful journey!</Timeline.Item>
-                            </Timeline>
-                        </FlexboxGrid.Item>
-                    </FlexboxGrid>
+                    {/* loop through posts */}
+                    {posts.map((post) => {
+                        return (<>
+                            <Col className='contentContainerCenterRow' xs={24} sm={12} md={8} lg={6}>
+                                <Panel shaded bordered bodyFill className='postListCard'>
+                                    <Image src={post.data.thumbnailUrl} width='500' height='500'/>
+                                    <Panel header={<Link
+                                        as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
+                                        href={`/posts/[slug]`}
+                                    >
+                                        <a>{post.data.title}</a>
+                                    </Link>}>
+                                        <small>{post.data.tags.map(tag => tag + ' ')}</small>
+                                        <p><small>
+                                            {post.data.date}
+                                            <br/>
+                                            {post.data.description}
+                                        </small></p>
+                                    </Panel>
+                                </Panel>
+                            </Col>
+                        </>)
+                    })}
                 </Row>
-
-            </FlexboxGrid.Item>
-
-        </FlexboxGrid>
-
+            </Row>
+        </Grid>
+        <Row className='modal-container'>
+            {/* Section - modal - content */}
+            <Modal open={open} onClose={handleClose}>
+                <Modal.Header>
+                    <Modal.Title>Contact Card</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <List bordered hover>
+                        <List.Item>Name: Joseph Dubon</List.Item>
+                        <List.Item>Title: Full-Stack Software Engineer</List.Item>
+                        <List.Item>Email: <a
+                            href='mailto:jd@josephdubon.com'>jd@josephdubon.com</a></List.Item>
+                        <List.Item>Phone: <a
+                            href='tel:(775)232-6280'>(775)232-6280</a></List.Item>
+                    </List>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleClose} appearance='primary'>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </Row>
     </Layout>)
 }
 
