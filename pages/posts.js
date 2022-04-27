a27be12aa5906554355c859f9a7d3ac6e127b1a9
Joseph Dubon <josephdubon@pm.me>
2022-04-27 14:56:10 -0700

    fixed order of posts

--- pages/posts.js
+++ pages/posts.js
@@ -15,7 +15,7 @@ export default function PostsHome({posts}) {
     posts = posts.filter(post => post.data.tags && post.data.tags.includes(filterTag))
 
     posts = posts.sort(function (a, b) {
-        return a - b // returns newest to oldest;change to a + b and returns oldest to newest
+        return a + b // returns newest to oldest; change to a - b and returns oldest to newest
     })
 
     return (<Layout>
